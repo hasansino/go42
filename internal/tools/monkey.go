39d4cda6dbbb53c7ec47ca59c8b59e3b6ce380df
hasansino <4024450+hasansino@users.noreply.github.com>
2025-06-21 17:42:10 +0300

    parallel docker build

--- internal/tools/monkey.go
+++ internal/tools/monkey.go
@@ -6,6 +6,7 @@ import (
 	"github.com/agiledragon/gomonkey/v2"
 )
 
+// nolint:unused
 // monkeyTime patches time.Now() function to return predefined value
 func monkeyTime(with time.Time) func() {
 	p := gomonkey.ApplyFunc(time.Now, func() time.Time {
